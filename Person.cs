fb51cd3ef0ec051256a497d149cc628a0428271e
gabbigum <gumata.gumata@gmail.com>
2020-11-17 17:48:47 +0200

    Person implemented

--- Person.cs
+++ Person.cs
@@ -3,29 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessObjects.Utilities;
 
 namespace BusinessObjects.Entities
 {
-    class Person
+    public class Person
     {
-        private String firstName;
-        private String lastName;
-        private String uniqueID; // generate unique id
-        
-        private String phoneNumber; // how many chars? 10
-
-        public Person(String firstName, String lastName, String phoneNumber)
-        {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.phoneNumber = phoneNumber;
-        }
-        
         public String FirstName { get; }
         public String LastName { get; }
 
-        public String UniqueID { get; }
+        public int UserID { get; }
         public String PhoneNumber { get; }
 
+
+        public Person(String firstName, String lastName, String phoneNumber)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.PhoneNumber = phoneNumber;
+            this.UserID = UIDGenerator.Instance.nextUniqueID();
+        }
+       
     }
 }
