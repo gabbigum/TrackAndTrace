caf40cf4f994f7e4c082dcefb00ea95856c30523
gabbigum <gumata.gumata@gmail.com>
2020-11-17 17:47:33 +0200

    singleton storage

--- DataStorage.cs
+++ DataStorage.cs
@@ -3,12 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataLayer.IO;
+using BusinessObjects.Entities;
+using BusinessObjects.TrackAndTrace;
 
 namespace DataLayer.Storage
 {
-    class DataStorage
+    public class DataStorage
     {
+        public static string DATA_FOLDER_PATH = @"C:\TrackAndTraceData";
 
+        public Dictionary<String, Person> Users { get; }
+
+        public Dictionary<String, Location> Locations { get; }
+
+        public Dictionary<String, List<UserEvent>> UserEvents{ get; }
+
+        private static DataStorage instance;
+
+        private DataStorage()
+        {
+            Users = new Dictionary<String, Person>();
+            Locations = new Dictionary<String, Location>();
+            UserEvents = new Dictionary<String, List<UserEvent>>();
+
+            //DataLoader loader = DataLoader.Instance.loadUserDataFromCSV();
+            //TODO on every start load all the data even if there is no data 
+
+            //TODO on every entry from the user write data and at the same time load to the collections
+        }
+
+        public static DataStorage Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new DataStorage();
+                }
+                return instance;
+            }
+        }
+
+
+        //might be useless
+        public List<UserEvent> getUserEventsByID(String userID)
+        {
+            if(!UserEvents.ContainsKey(userID))
+            {
+                throw new ArgumentException("The user does not exist.");
+            }
+
+            return UserEvents[userID];
+        }
 
     }
 }
