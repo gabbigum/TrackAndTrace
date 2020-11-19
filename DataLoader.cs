d2547becc42ef906e8951d6739701d1e326520e1
gabbigum <gumata.gumata@gmail.com>
2020-11-19 09:26:52 +0200

    load core data

--- DataLoader.cs
+++ DataLoader.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using BusinessObjects.Entities;
 using DataLayer.Storage;
+using BusinessObjects.TrackAndTrace;
 
 namespace DataLayer.IO
 {
@@ -77,7 +78,7 @@ namespace DataLayer.IO
             {
                 String line = reader.ReadLine();
                 string[] values = line.Split(',');
-                
+
                 dataStorage.Locations.Add(
                     values[0],
                     new Location(
@@ -85,13 +86,57 @@ namespace DataLayer.IO
                         values[2],
                         values[3]));
             }
-
             return true;
         }
 
+        public void loadUserEventsFromCSV(string fileName)
+        {
+            //start reading form file
+            //for every line check if there's person 2
+            //if there is load the data for the person 2 in the storage as well
+            DataStorage storage = DataStorage.Instance;
 
-        //load data from events.csv
-        //load data from users.csv
-        //load data from locations.csv
+            StreamReader reader = new StreamReader(File.OpenRead(fileName));
+
+            string header = reader.ReadLine();
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] values = line.Split(',');
+
+                if (values[1].Equals("empty"))
+                {
+                    storage.UserEvents.Add(new UserEvent(loadPersonByID(values[0]), values[2]));
+                    //verify format
+                } else
+                {
+                    storage.UserEvents.Add(new UserEvent(loadPersonByID(values[0]),loadPersonByID(values[1]), values[2]));
+                }
+            }
+        }
+
+        private Person loadPersonByID(string userID)
+        {
+            DataStorage storage = DataStorage.Instance;
+
+            StreamReader reader = new StreamReader(File.OpenRead(DataStorage.USER_EVENTS_FILE));
+
+            string header = reader.ReadLine();
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] values = line.Split(',');
+
+                if (values[0].Equals(userID))
+                {
+                    return new Person(values[1], values[2], values[3]);
+                }
+            }
+            throw new ArgumentException("User id not found.");
+        }
+
+
     }
 }
