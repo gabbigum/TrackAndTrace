88f772e6e90d87a5c7a481d7dd7bbaf54d34f21e
gabbigum <gumata.gumata@gmail.com>
2020-11-19 16:43:58 +0200

    constructors locID parameter

--- UserEvent.cs
+++ UserEvent.cs
@@ -11,26 +11,26 @@ namespace BusinessObjects.TrackAndTrace
     {
         private Person Person1 { get; }
         private Person Person2 { get; }
-        private String Location { get; }
+        private String LocationID { get; }
         private DateTime DateTime { get; }
 
         private String ToString;
 
 
-        public UserEvent(Person person, String location)
+        public UserEvent(Person person, String locationID)
         {
             this.Person1 = person;
-            this.Location = location;
+            this.LocationID = locationID;
             DateTime = DateTime.Now;
-            ToString = Person1.UserID + "," + Location + "," + DateTime.ToString();
+            ToString = Person1.UserID + "," + LocationID + "," + DateTime.ToString();
         }
-        public UserEvent(Person person1, Person person2, String location)
+        public UserEvent(Person person1, Person person2, String locationID)
         {
             this.Person1 = person1;
             this.Person2 = person2;
-            this.Location = location;
+            this.LocationID = locationID;
             this.DateTime = DateTime.Now;
-            ToString = Person1.UserID + ","+ Person2.UserID + "," + Location + "," + DateTime.ToString();
+            ToString = Person1.UserID + ","+ Person2.UserID + "," + LocationID + "," + DateTime.ToString();
         }
 
         public string toString()
