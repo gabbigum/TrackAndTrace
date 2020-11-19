82b20e7e58c37237babf7e53514d59033c1c6635
gabbigum <gumata.gumata@gmail.com>
2020-11-19 12:15:15 +0200

    toString() added

--- UserEvent.cs
+++ UserEvent.cs
@@ -14,11 +14,15 @@ namespace BusinessObjects.TrackAndTrace
         private String Location { get; }
         private DateTime DateTime { get; }
 
+        private String ToString;
+
+
         public UserEvent(Person person, String location)
         {
             this.Person1 = person;
             this.Location = location;
             DateTime = DateTime.Now;
+            ToString = Person1.UserID + "," + Location + "," + DateTime.ToString();
         }
         public UserEvent(Person person1, Person person2, String location)
         {
@@ -26,11 +30,17 @@ namespace BusinessObjects.TrackAndTrace
             this.Person2 = person2;
             this.Location = location;
             this.DateTime = DateTime.Now;
+            ToString = Person1.UserID + ","+ Person2.UserID + "," + Location + "," + DateTime.ToString();
         }
 
+        public string toString()
+        {
+            return ToString;
+        }
 
         public bool recordEvent()
         {
+            //send message to datawriter
             return false;
         }
 
