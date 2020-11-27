0d9f30de43e0844f959acae75202d913f72f4f6c
gabbigum <gumata.gumata@gmail.com>
2020-11-27 09:00:27 +0200

    load all data

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using BusinessObjects.TrackAndTrace;
 using BusinessObjects.Entities;
-
+using DataLayer.IO;
 
 namespace TrackAndTrace
 {
@@ -25,13 +25,20 @@ namespace TrackAndTrace
     public partial class MainWindow : Window
     {
         private TrackAndTraceService trackAndTrace;
+        private DataLoader loader;
         private const int NAME_LENGTH = 30;
         private const int PHONE_LENGTH = 10;
+
 
         public MainWindow()
         {
             InitializeComponent();
             trackAndTrace = new TrackAndTraceService();
+            loader = DataLoader.Instance;
+            loader.loadAllData();
+
+            //populate comboboxes method below maybe???
+            populateComboBoxes();
 
         }
 
@@ -60,7 +67,7 @@ namespace TrackAndTrace
 
             if (txtInsertPhoneNumber.Text.Length != PHONE_LENGTH)
             {
-                MessageBox.Show("Invalid phone number. You need to enter 10 digits.");
+                MessageBox.Show("Invalid phone number. You need to enter 10 digits number.");
                 return;
             }
 
@@ -106,6 +113,19 @@ namespace TrackAndTrace
                 MessageBox.Show("You must fill in all the fields. Please try again.");
                 return;
             }
+            if (txtInsertFirstName.Text.Length > NAME_LENGTH ||
+    txtInsertLastName.Text.Length > NAME_LENGTH)
+            {
+                MessageBox.Show("Invalid name. Too many characters.");
+                return;
+            }
+            if (txtInsertLocationName.Text.Length > 50 ||
+                txtInsertLocationType.Text.Length > 15 ||
+                txtInsertAddress.Text.Length > 100)
+            {
+                MessageBox.Show("Invalid entry. You cannot have input that long.");
+                return;
+            }
 
             if (Regex.IsMatch(txtInsertLocationType.Text, @"\d"))
             {
@@ -128,6 +148,36 @@ namespace TrackAndTrace
             MessageBox.Show("Location added successfully.");
         }
 
+        private void btnRecordContact_Click(object sender, RoutedEventArgs e)
+        {
+            //user enters data - validations happen here
+            //method from track and trace invokes - recordContact()
+            //writes data to the file
+            //loads object to storage -- should that be coupled into addNewLocation()???
 
+            //needs to check if the contacts and location exist
+
+  //          String personOneID = cboSelectContactPersonOne.SelectedItem.ToString().Split(' ')[2];
+//            MessageBox.Show(personOneID);
+
+            String personOneID = cboSelectContactPersonOne.SelectedItem.ToString().Split(' ')[2];
+
+//            bool recordContact = trackAndTrace.recordContact(loader.loadPersonByID(personOneID), );
+            MessageBox.Show("Contact added successfully.");
+        }
+
+        private void populateComboBoxes()
+        {
+            foreach (KeyValuePair<String, Person> entry in trackAndTrace.dataStorage.Users)
+            {
+                cboSelectContactPersonOne.Items.Add(entry.Value.FirstName + " " + entry.Value.LastName + " " + entry.Key);
+                cboSelectContactPersonTwo.Items.Add(entry.Value.FirstName + " " + entry.Value.LastName + " " + entry.Key);
+            }
+
+            foreach (KeyValuePair<String, Location> entry in trackAndTrace.dataStorage.Locations)
+            {
+                cboSelectContactLocation.Items.Add(entry.Value.LocationName + " " + entry.Key);
+            }
+        }
     }
 }
